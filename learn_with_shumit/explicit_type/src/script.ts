f80ec17153badcbc7cd306a99faaf51106616635
sabirul-islam <sabirul.shimul@gmail.com>
2021-02-12 04:46:57 +0600

    explicit type practice

---
+++ learn_with_shumit/explicit_type/src/script.ts
@@ -0,0 +1,38 @@
+// // explicit data type
+// let a: string
+// let b: number
+// a = 1
+// b = '34'
+
+// // normal variable
+// let b: string | number
+// b="sabirul"
+// b=34
+
+// // array
+// let c: string[]=[]
+// c.push('1')
+// console.log(c);
+// let a: (string | number)[]=["montu", "monju"]
+// a.push('shumit', 33)
+// console.log(a);
+
+// // object
+// let d:object;
+// d={
+//     // any type of data
+// }
+
+// let e:object
+// e=[1,2,3] //array is one kind of object
+
+// let c:{
+//     name:string,
+//     age:number,
+//     adult:boolean
+// }
+// c={
+//     name:"shimul",
+//     age:"33",
+//     adult:true
+// }
