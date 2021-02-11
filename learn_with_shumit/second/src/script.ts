532cd021b9d5224c974d3f9f6cc337ac011d11b9
sabirul-islam <sabirul.shimul@gmail.com>
2021-02-12 04:07:56 +0600

    first typescript practice

---
+++ learn_with_shumit/second/src/script.ts
@@ -0,0 +1,49 @@
+// console.log("hello");
+
+// const country = "Bangladesh"
+// console.log(country);
+
+// //Data Type
+// let playerName = "Mashrafi"
+// let age = 33
+// console.log(playerName);
+// age = "sabirul"
+// console.log(playerName);
+
+// let playerName
+// console.log(playerName);
+// playerName="ami"
+// playerName=34
+// console.log(playerName);
+
+// //function
+// function multiply(a:number,b:number){
+//     return a*b
+// }
+// console.log(multiply("ami",5));
+
+// let fruits = ["apple", "banana", "orange"]
+// fruits.push(34)
+// console.log(fruits);
+
+// let fruits = []
+// fruits.push(34,"shimul")
+// console.log(fruits);
+
+// // Array
+// let mixed = ["apple", 3, true,{}]
+// mixed.push("kola",22,false)
+// mixed.push({
+//     name:"aminul"
+// })
+// console.log(mixed);
+
+// Object
+let person ={
+    name:"mashrafi",
+    age:35,
+    isCaptain:true
+}
+person.name=56
+person.country = "Bangladesh"
+console.log(person);
