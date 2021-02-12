d5e317f8ed14ac8c0a25580dfbee46a6e123d954
sabirul-islam <sabirul.shimul@gmail.com>
2021-02-12 20:19:36 +0600

    any type

---
+++ learn_with_shumit/any_type/script.ts
@@ -0,0 +1,21 @@
+// any type data
+let a: any
+a=5
+a="shimul"
+
+// any type array
+let b: any[] = []
+b.push(34)
+b.push("shimul")
+
+// any type object
+let c: {
+    name: any,
+    age: any
+}
+
+c = {
+    name: 'shimul',
+    age: 45
+}
+c.age = '45'
