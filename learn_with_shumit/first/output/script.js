ebf915a0c52a33dce3ad3385f69958c550951986
sabirul-islam <sabirul.shimul@gmail.com>
2021-02-12 20:49:54 +0600

    any type function

--- learn_with_shumit/first/output/script.js
+++ learn_with_shumit/first/output/script.js
@@ -1,1 +1,25 @@
 "use strict";
+// //function type
+// let myFunc: Function
+// myFunc = ()=>{
+//     console.log("hello");
+// }
+// myFunc = 5
+// // Function Parameter
+// const myFunc = (a:string, b:string, c?:string) => {
+//     console.log(c);
+//     console.log(`hello ${a} ${b}`);
+// }
+// myFunc('sabirul', 'islam')
+// // Function Parameter with default value
+// const myFunc = (a:string, b:string, c:string = 'ami') => {
+//     console.log(c);
+//     console.log(`hello ${a} ${b}`);
+// }
+// myFunc('sabirul', 'islam')
+// return void
+var myFunc = function (a, b, c) {
+    if (c === void 0) { c = 'ami'; }
+    return a + b;
+};
+myFunc('sabirul', 'islam');
